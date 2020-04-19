9c59300810e7dbbc8ae19931ab20209bb618a865
orchie lucas <orchie.lucas@gmail.com>
2020-04-19 10:47:36 -0500

    Add auth module

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import './App.css';
-import DisplayPosts from './components/DisplayPosts';
-import CreatePost from './components/CreatePost';
+import React from "react";
+import "./App.css";
+import DisplayPosts from "./components/DisplayPosts";
+import CreatePost from "./components/CreatePost";
+import { withAuthenticator } from "aws-amplify-react";
 
 function App() {
   return (
     <div className="App">
       <CreatePost />
-      <DisplayPosts/>
+      <DisplayPosts />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App, { includeGreetings: true });
