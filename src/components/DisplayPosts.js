9c3130f80dda38886628e271da32a275e5527416
orchie lucas <orchie.lucas@gmail.com>
2020-04-19 10:33:33 -0500

    Add onCreate subscription and refreshing ui with posts automatically

--- src/components/DisplayPosts.js
+++ src/components/DisplayPosts.js
@@ -3,6 +3,7 @@ import { listPosts } from "../graphql/queries";
 import { API, graphqlOperation } from "aws-amplify";
 import DeletePost from "./DeletePost";
 import EditPost from "./EditPost";
+import { onCreatePost } from "../graphql/subscriptions";
 
 class DisplayPosts extends Component {
   state = {
@@ -11,7 +12,32 @@ class DisplayPosts extends Component {
 
   componentDidMount = async () => {
     this.getPosts();
+    //Get all the post data once a new post is created
+    this.createPostListener = API.graphql(
+      graphqlOperation(onCreatePost)
+    ).subscribe({
+      next: (postData) => {
+        //Create a copy of the posts array only with the newly created posts
+        const newPost = postData.value.data.onCreatePost;
+        //Create a post array for previous posts only
+        //Make sure in our previous/state post array we don't have the new post
+        const prevPosts = this.state.posts.filter(
+          (post) => post.id !== newPost.id
+        );
+
+        //Concatenate the new and the previous copy of the post array
+        const updatedPosts = [newPost, ...prevPosts];
+        //Update the posts array
+        this.setState({ posts: updatedPosts });
+      },
+    });
   };
+
+
+  //Unmount the subscription listener
+  componentWillUnmount(){
+    this.createPostListener.unsubscribe();
+  }
 
   getPosts = async () => {
     const result = await API.graphql(graphqlOperation(listPosts));
